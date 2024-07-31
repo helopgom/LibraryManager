55e666fa533743a438187889af150342abbe296b
Fire-Fairy84 <esther.sarasua@gmail.com>
2024-07-31 20:15:28 +0200

    check_duplicate_book function ready

--- main.py
+++ main.py
@@ -1,84 +1,34 @@
-from config import DbConnection
 from models.BooksModel import BooksModel
-from config.DbConnection import Connection
-
-
 
 # Instancia del modelo BooksModel
 book = BooksModel()
 
 # Datos del libro de ejemplo
 data = {
-    'title': "Aire",
-    'author': "Maria Suarez",
-    'isbn': "5698569",
-    'year_edition': "2023-12-12",
-    'category_id_categories': 2
+    'title': "La  moderna",
+    'author': "Francisco",
+    'isbn': "9785550",
+    'year_edition': "2013-01-12",
+    'category_id_categories': 3
 }
 
-# Función para crear un libro
-def create_book():
-    add_book_id = book.create(book.table_name, data)
-    if add_book_id:
-        print(f'Libro insertado con éxito.')
-    else:
-        print(f'Error al insertar el libro.')
 
+# Ejemplos de uso
 
-
-# Función para actualizar los datos de un libro (requiere criterios y datos a actualizar)
-def update_book_data(criteria, new_data):
-    update_book_result = book.update(book.table_name, new_data, criteria)
-    if update_book_result:
-        print(f'Libro actualizado con éxito.')
-    else:
-        print(f'Error al actualizar el libro.')
-
-
-# Función para eliminar un libro (requiere un criterio)
-def delete_book(criteria):
-    delete_book_result = book.delete(book.table_name, criteria)
-    if delete_book_result:
-        print(f'Libro eliminado con éxito.')
-    else:
-        print(f'Error al eliminar el libro.')
-
-
-# Función para consultar o filtrar la información
-
-def query_books(criteria=None):
-
-    if criteria:
-        criteria_str = ", ".join([f"{key} = {value}" for key, value in criteria.items()])
-        print(f"\nConsultando libros con criterio/s: '{criteria_str}'")
-    else:
-        print("\nConsultando todos los libros.")
-
-    query_books_result = book.read(book.table_name, criteria)
-    if query_books_result:
-        for row in query_books_result:
-            print(row)
-    else:
-        print("No se encontraron resultados.")
-
-
-
-# Ejemplo de uso
 if __name__ == "__main__":
-    # #Crear libro
-    # create_book()
+    # Crear libro
+      # book.create_book(data)
 
     # # Criterios para actualizar un libro y los nuevos datos
-    # update_criteria = {'title': 'Fuego'}
-    # new_data = {'title': 'Fuego', 'author': 'Juan Moreno', 'year_edition': '2013-01-01'}
-    # update_book_data(update_criteria, new_data)
+    # update_criteria = {'book_id': 22}
+    # new_data = {'author': 'Rita'}
+    # book.update_book_data(update_criteria, new_data)
 
     # # Eliminar un libro con un criterio específico (por ejemplo, por ISBN)
-    # delete_criteria = {'title': 'Tierra'}
-    # delete_book(delete_criteria)
+    # delete_criteria = {'book_id': 21}
+    # book.delete_book(delete_criteria)
 
-    # # Consultar todos los libros
-    # print("Consultando todos los libros:")
-    # query_books()
+    # Consultar todos los libros
+    book.query_books({'book_id': 23})
 
 
--- models/BooksModel.py
+++ models/BooksModel.py
@@ -1,6 +1,62 @@
 from models.GeneralModel import GeneralModel
 
+
 class BooksModel(GeneralModel):
     def __init__(self):
         super().__init__()
         self.table_name = 'books'
+
+
+    # Función para crear un libro
+    def create_book(self, data):
+        isbn = data.get('isbn')
+
+        if self.check_duplicate_book(isbn):
+            print(f'El libro con el ISBN {isbn} ya existe.')
+        else:
+            add_book = self.create(self.table_name, data)
+            if add_book:
+                print(f'Libro insertado con éxito.')
+            else:
+                print(f'Error al insertar el libro.')
+
+
+    # Función para comprobar si un libro ya existe
+    def check_duplicate_book(self, isbn):
+        results = self.read(self.table_name, {'isbn': isbn})
+        return bool(results)
+
+
+    # Función para actualizar los datos de un libro (requiere criterios y datos a actualizar)
+    def update_book_data(self, criteria, new_data):
+        update_book_result = self.update(self.table_name, new_data, criteria)
+        if update_book_result:
+            print(f'Libro actualizado con éxito.')
+        else:
+            print(f'Error al actualizar el libro.')
+
+
+    # Función para eliminar un libro (requiere un criterio)
+    def delete_book(self, criteria):
+        delete_book_result = self.delete(self.table_name, criteria)
+        if delete_book_result:
+            print(f'Libro eliminado con éxito.')
+        else:
+            print(f'Error al eliminar el libro.')
+
+
+    # Función para consultar o filtrar la información
+    def query_books(self, criteria=None):
+        if criteria:
+            criteria_str = ", ".join([f"{key} = {value}" for key, value in criteria.items()])
+            print(f"\nConsultando libros con criterio/s: '{criteria_str}'")
+        else:
+            print("\nConsultando todos los libros.")
+
+        query_books_result = self.read(self.table_name, criteria)
+        if query_books_result:
+            for row in query_books_result:
+                print(row)
+        else:
+            print("No se encontraron resultados.")
+
