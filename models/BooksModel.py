b875882215df9b851a3aa596da9b29321292ee48
Fire-Fairy84 <esther.sarasua@gmail.com>
2024-08-08 15:13:43 +0200

    create log configuration and replace prints in BooksModel

--- models/BooksModel.py
+++ models/BooksModel.py
@@ -1,5 +1,6 @@
 import psycopg2
 from models.GeneralModel import GeneralModel
+import logging
 
 
 class BooksModel(GeneralModel):
@@ -13,15 +14,15 @@ class BooksModel(GeneralModel):
 
         try:
             if self.check_duplicate_book(isbn):
-                print(f'El libro con el ISBN {isbn} ya existe.')
+                logging.info(f'El libro con el ISBN {isbn} ya existe.')
             else:
                 add_book = self.create(self.table_name, data)
                 if add_book:
-                    print(f'Libro insertado con éxito.')
+                    logging.info(f'Libro insertado con éxito.')
                 else:
-                    print(f'Error al insertar el libro.')
+                    logging.error(f'Error al insertar el libro.')
         except psycopg2.Error as e:
-            print(f"Error al crear el libro: {e}")
+            logging.error(f"Error al crear el libro: {e}")
 
     # Función para comprobar si un libro ya existe
     def check_duplicate_book(self, isbn):
@@ -29,44 +30,44 @@ class BooksModel(GeneralModel):
             results = self.read(self.table_name, {'isbn': isbn})
             return bool(results)
         except psycopg2.Error as e:
-            print(f"Error al crear el libro: {e}")
+            logging.error(f"Error al crear el libro: {e}")
 
     # Función para actualizar los datos de un libro (requiere criterios y datos a actualizar)
     def update_book_data(self, criteria, new_data):
         try:
             update_book_result = self.update(self.table_name, new_data, criteria)
             if update_book_result:
-                print(f'Libro actualizado con éxito.')
+                logging.info(f'Libro actualizado con éxito.')
             else:
-                print(f'Error al actualizar el libro.')
+                logging.error(f'Error al actualizar el libro.')
         except psycopg2.Error as e:
-            print(f"Error al actualizar los datos: {e}")
+            logging.error(f"Error al actualizar los datos: {e}")
 
     # Función para eliminar un libro (requiere un criterio)
     def delete_book(self, criteria):
         try:
             delete_book_result = self.delete(self.table_name, criteria)
             if delete_book_result:
-                print(f'Libro eliminado con éxito.')
+                logging.info(f'Libro eliminado con éxito.')
             else:
-                print(f'Error al eliminar el libro.')
+                logging.error(f'Error al eliminar el libro.')
         except psycopg2.Error as e:
-            print(f"Error al eliminar el libro: {e}")
+            logging.error(f"Error al eliminar el libro: {e}")
 
     # Función para consultar o filtrar la información
     def query_books(self, criteria=None):
         try:
             if criteria:
                 criteria_str = ", ".join([f"{key} = {value}" for key, value in criteria.items()])
-                print(f"\nConsultando libros con criterio/s: '{criteria_str}'")
+                logging.info(f"\nConsultando libros con criterio/s: '{criteria_str}'")
             else:
-                print("\nConsultando todos los libros.")
+                logging.info("\nConsultando todos los libros.")
 
             query_books_result = self.read(self.table_name, criteria)
             if query_books_result:
                 for row in query_books_result:
                     print(row)
             else:
-                print("No se encontraron resultados.")
+                logging.info("No se encontraron resultados.")
         except psycopg2.Error as e:
-            print(f"Error en la consulta: {e}")
+            logging.error(f"Error en la consulta: {e}")
