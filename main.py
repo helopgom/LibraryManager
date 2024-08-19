fe709081c06afe61685ec2d1e4013f4d59ce8587
Fire-Fairy84 <esther.sarasua@gmail.com>
2024-08-19 23:37:02 +0200

    add methods to main.py

--- main.py
+++ main.py
@@ -1,25 +1,88 @@
 from models.BooksModel import BooksModel
 from models.LibraryLoansModel import LibraryLoans
+from models.UsersModel import UsersModel
+from models.CategoriesModel import CategoriesModel
+import logging
+from logging_config import setup_logging
+
+
 
 library_loan = LibraryLoans()
-data = {
+book = BooksModel()
+user = UsersModel()
+categories_model = CategoriesModel()
+
+data_loan = {
     'book_id_books': 1,
     'user_id_users': 2,
     'entry_date': "2013-01-05",
     'return_date': "2013-01-12"
 }
 
-book = BooksModel()
-data2 = {
-    'title': "nuevo libro",
-    'author': "Esther",
-    'isbn': "9100522",
-    'year_edition': "2013-01-12",
-    'category_id_categories': 1
+data_book = {
+    'title': "Normal People",
+    'author': "Sally Rooney",
+    'isbn': "190011123",
+    'year_edition': "2018-01-12",
+    'category_id_categories': 2
 }
-if __name__ == "__main__":
-    #book.create_book(data2)
-    #library_loan.create_loan(3,2,"2013-06-05","2013-08-12")
-    library_loan.read_loan()
 
+new_user_data = {
+    "dni": "13456879X",
+    "user_name": "María",
+    "user_lastname": "Estevez",
+    "mail": "maria.estevez@example.com",
+    "phone": "987654321"
+}
+# new_user_data = {
+#     "dni": "87654321B",
+#     "user_name": "Raquel",
+#     "user_lastname": "Casado",
+#     "mail": "raquelcasado@gmail.com",
+#     "phone": "123456789"
+# }
 
+category_data = {
+    'category_id': 1,
+    'category_name': 'Ficción'
+}
+
+# categories_model.create_category(3, 'Poesía')
+
+# categories_model.search_and_filter(1)
+
+# categories_model.check_category(2)
+#
+# categories_model.update_category(3, 'Teatro')
+#
+# categories_model.delete_category(3)
+
+#
+# user.create_user(new_user_data)
+
+# user.check_user()
+#
+# user.update_user()
+#
+# user.delete_user()
+#
+# user.search_users()
+#
+# if __name__ == "__main__":
+# # Crear libro
+# book.create_book(data_book)
+
+# # Criterios para actualizar un libro y los nuevos datos
+# update_criteria = {'category_id_categories': 3}
+# new_data = {'category_id_categories': 1}
+# book.update_book_data(update_criteria, new_data)
+
+# # Eliminar un libro con un criterio específico (por ejemplo, por ISBN)
+# delete_criteria = {'book_id': 21}
+# book.delete_book(delete_criteria)
+
+# # Consultar todos los libros
+# book.query_books({'title': 'titulo consultado'})
+
+# # Crear nuevo préstamo
+#     library_loan.create_loan(3, 1, "2022-07-19", "2022-03-25")
--- src/controllers/UserController.py
+++ src/controllers/UserController.py
@@ -6,9 +6,7 @@ class UserController:
         self.user_model = UsersModel()
 
     def check_user(self, data):
-        """Verify if a user with the same ID or email already exists."""
         try:
-            # Verifica el usuario usando el método del modelo
             verification_message = self.user_model.check_user(data)
             if verification_message:
                 return dict(status_code=400, response=verification_message)
@@ -17,14 +15,11 @@ class UserController:
             return dict(status_code=500, response='Internal server error: ' + str(e))
 
     def create_user(self, data):
-        """Create a new user after verifying that there are no duplicates."""
         try:
-            # Verificar si ya existe un usuario con el mismo DNI o correo
             verification_response = self.check_user(data)
             if verification_response['status_code'] != 200:
                 return verification_response
 
-            # Crear el nuevo usuario si la verificación fue exitosa
             result = self.user_model.create_user(data)
             if result:
                 return dict(status_code=201, response='User created successfully.')
